c4ffa1181172f95bce3306d9e2b2cd69857edcbf
Martin Martinez <investingmtz@gmail.com>
2020-11-18 23:52:42 -0600

    Agregando funcion de movimiento

--- snake.py
+++ snake.py
@@ -1,4 +1,7 @@
 import turtle
+import time
+
+POSPONER = 0.1
 
 #Configuracion ventana
 ventana = turtle.Screen()
@@ -12,3 +15,31 @@ serpiente = turtle.Turtle()
 serpiente.speed(0)
 serpiente.shape('square')
 serpiente.penup()
+serpiente.goto(0,0)
+serpiente.direction = 'left'
+serpiente.color('white')
+
+#Funciones
+def movimiento():
+    if serpiente.direction == 'up':
+        y = serpiente.ycor()
+        serpiente.sety(y + 20)
+
+    if serpiente.direction == 'down':
+        y = serpiente.ycor()
+        serpiente.sety(y - 20)
+    
+    if serpiente.direction == 'right':
+        x = serpiente.xcor()
+        serpiente.setx(x + 20)
+    
+    if serpiente.direction == 'left':
+        x = serpiente.xcor()
+        serpiente.setx(x - 20)
+
+
+while True:
+    ventana.update()
+    movimiento()
+
+    time.sleep(POSPONER)
